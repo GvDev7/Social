d4b4a73f054bc1533bd27af4708288a034cee466
Aaron <a.genevision14@gmail.com>
2024-04-04 09:19:17 -0500

    Initial project commit. Created mysql database, connected to mysql database, and created registration page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+$con = mysqli_connect("localhost","root","", "social");  // Establishing connection with server name, username and password.
+if(mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+$query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'GVDev')");
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GV-World</title>
+</head>
+<body>
+    <h1>Welcome to GV World!</h1>
+</body>
+</html>
